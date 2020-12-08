5661dc28b1cb89b400b296ad8cee1178866f8e48
Joshua Smerdel <josh@smerdel.net>
2020-12-07 21:28:30 -0500

    m01d05: Temp Conversion - Complete
    Completed with Justin Frey & Justin B using ideas introduced by Jeff

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,33 @@
 package com.techelevator;
 
-public class TempConvert {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class TempConvert
+{
 
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is the temperature you would like to convert?");
+		String tempToConvert = scanner.nextLine();
+		double tempAnswer = Double.parseDouble(tempToConvert);	
+		
+		System.out.println("Is your measurement Fahrenheit or Celsius?");
+		String degreesOrCelsius = scanner.nextLine();
+		
+		if (degreesOrCelsius.toUpperCase().startsWith("F"))
+		{
+			double farToCel = ((tempAnswer - 32) / 1.8); 
+			System.out.println(tempAnswer + "F" + " converts to " + farToCel + " Celsius" );
+		}
+		if (degreesOrCelsius.toUpperCase().startsWith("C"))
+		{
+			double celToFar = ((tempAnswer * 1.8) + 32);
+			System.out.println(tempAnswer + "C" + " converts to " + celToFar + " Fahrenheit" );
+		}
+	
+				
 	}
 
 }
