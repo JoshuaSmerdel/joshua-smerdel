7e5bbb2cd13cc7f088dec6b22d510ecf230973a2
Joshua Smerdel <josh@smerdel.net>
2020-12-21 19:04:48 -0500

    Review Exercise 5: Command Line Programs - Fibonacci - Completed

--- review-exercises/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ review-exercises/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,34 @@
 package com.techelevator;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 
-	}
+		Scanner input = new Scanner(System.in);
+		int sum = 0;
+		int firstNum = 0;
+		int secondNum = 1;
+		String sequenceToUpperBound = "0, 1";
 
+		System.out.println("Please enter the Fibonacci number: ");
+
+		int upperBoundNum = input.nextInt();
+
+		System.out.print(sequenceToUpperBound);
+		while (sum < upperBoundNum) {
+			sum = firstNum + secondNum;
+
+			if (sum < upperBoundNum) {
+				System.out.print(", " + sum);
+				firstNum = secondNum;
+				secondNum = sum;
+				
+			}
+		}
+
+		System.out.println();
+	}
 }
