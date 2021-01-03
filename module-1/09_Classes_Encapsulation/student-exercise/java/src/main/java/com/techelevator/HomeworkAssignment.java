f421764958145cf163c1476eb3ac96d70633f1a7
Joshua Smerdel <josh@smerdel.net>
2021-01-03 16:59:46 -0500

    m01d09: Encapsulation/Classes - Complete

--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,63 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+	private int totalMarks;
+	private int possibleMarks;
+	private String submitter;
+
+	public HomeworkAssignment(int totMark, int posMark, String submit) {
+		totMark = 0;
+		totalMarks = totMark;
+		posMark = 100;
+		possibleMarks = posMark;
+		submit = "";
+		submitter = submit;
+	}
+
+	public int getTotalMarks() {
+		return totalMarks;
+	}
+
+	public void setTotalMarks(int totalMarks) {
+		if (totalMarks <= possibleMarks && totalMarks >= 0) {
+			this.totalMarks = totalMarks;
+		}
+	}
+
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
+
+	public void setPossibleMarks(int possibleMarks) {
+		if (possibleMarks > 0) {
+			this.possibleMarks = possibleMarks;
+		}
+	}
+
+	public String getSubmitter() {
+		return submitter;
+	}
+
+	public void setSubmitter(String submitter) {
+		if (submitter != null && !submitter.isEmpty()) {
+			this.submitter = submitter;
+		}
+	}
+	
+	public String getLetterGrade() {
+		double percentage = (double) totalMarks / possibleMarks;
+		
+		if (percentage >= 0.9) {
+			return "A";
+		} else if (percentage >= 0.8) {
+			return "B";
+		} else if (percentage >= 0.7) {
+			return "C";
+		} else if (percentage >= 0.6) {
+			return "D";
+		} else {
+			return "F";
+		}
+	}
 
 }
