b83b48d4e99fef46ab71f4a2b72b593f36e45a94
Joshua Smerdel <josh@smerdel.net>
2021-01-03 17:52:37 -0500

    m01d11: Inheritance

---
+++ module-1/11_Inheritance/classes-introduction-review/java/src/main/java/com/techelevator/cardexample/Program.java
@@ -0,0 +1,25 @@
+package com.techelevator.cardexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+	public void main(String[] args) {
+		
+		String card1Suit = "Spades";
+		String card1FaceValue = "Ace";
+		int card1Value = 14;
+		boolean card1FaceUp = false;
+		String card1Color = "Black";
+		
+		List<String> cardSuit = new ArrayList<>();
+		List<Integer> cardValues = new ArrayList<>();
+		List<String> cardFaceValues = new ArrayList<>();
+		List<Boolean> cardsAreFaceUp = new ArrayList<>();
+		List<String> cardColors = new ArrayList<>();
+		
+		
+		
+	}
+}
