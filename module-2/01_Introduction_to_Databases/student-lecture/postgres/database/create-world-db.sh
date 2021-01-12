aee9aecf6260ba566b522755afb917ceb3001e40
Joshua Smerdel <josh@smerdel.net>
2021-01-12 09:39:59 -0500

    edits to gregors bash script file

--- module-2/01_Introduction_to_Databases/student-lecture/postgres/database/create-world-db.sh
+++ module-2/01_Introduction_to_Databases/student-lecture/postgres/database/create-world-db.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 psql -U postgres -f "./dropdb.sql" &&
+
+user: postgres
+password: postgres1
+
+
+
+
 createdb -U postgres world &&
+
+
+
+
+
+
 psql -U postgres -d world -f "./world.sql" 
