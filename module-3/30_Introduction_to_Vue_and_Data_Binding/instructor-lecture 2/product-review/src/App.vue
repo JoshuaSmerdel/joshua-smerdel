25f27e0846837d60f7a7d47d2120e6b825bede7a
Joshua Smerdel <josh@smerdel.net>
2021-02-27 15:08:33 -0500

    m03d30: Introduction to VUE - Instructor Lecture

---
+++ module-3/30_Introduction_to_Vue_and_Data_Binding/instructor-lecture 2/product-review/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <product-reviews />
+  </div>
+</template>
+
+<script>
+
+import ProductReviews from "@/components/ProductReviews";
+export default {
+  name: 'App',
+  components: {
+    ProductReviews
+  }
+}
+</script>
+
+<style>
+
+div.main div.well-display {
+  display: flex;
+  justify-content: space-around;
+}
+
+div.main div.well-display div.well {
+  display: inline-block;
+  width: 15%;
+  border: 1px black solid;
+  border-radius: 6px;
+  text-align: center;
+  margin: 0.25rem;
+}
+
+div.main div.well-display div.well span.amount {
+  color: darkslategray;
+  display: block;
+  font-size: 2.5rem;
+}
+
+div.main div.review {
+  border: 1px black solid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+}
+
+div.main div.review div.rating {
+  height: 2rem;
+  display: inline-block;
+  vertical-align: top;
+  margin: 0 0.5rem;
+}
+
+div.main div.review div.rating img {
+  height: 100%;
+}
+
+div.main div.review p {
+  margin: 20px;
+}
+
+div.main div.review h3 {
+  display: inline-block;
+}
+
+div.main div.review h4 {
+  font-size: 1rem;
+}
+
+div.main div.review.favorite {
+  background-color: lightyellow;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ module-3/30_Introduction_to_Vue_and_Data_Binding/instructor-lecture 2/product-review/src/components/ProductReviews.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="main">
+    <h1>Product Reviews for ...{{ title }}</h1>
+    <p>{{ description }}</p>
+
+<!--    add the summary wells here-->
+    <div class="well-display">
+      <div class="well">
+        <span class="amount">{{averageRating}}</span>
+        Average Rating
+      </div>
+      <div class="well">
+        <span class="amount"></span>
+        1 Star Review
+      </div>
+      <div class="well">
+        <span class="amount">...</span>
+        2 Star Review
+      </div>
+      <div class="well">
+        <span class="amount">...</span>
+        3 Star Review
+      </div>
+      <div class="well">
+        <span class="amount">...</span>
+        4 Star Review
+      </div>
+      <div class="well">
+        <span class="amount">...</span>
+        5 Star Review
+      </div>
+    </div>
+
+    <!--Loop through all reviews-->
+    <div class="review"
+         v-for="review in reviews"
+         v-bind:key="review.id">
+      <h4>
+        {{ review.reviewer }}
+      </h4>
+      <div class="rating">
+        <img src="../assets/star.png" alt="star image" v-for="n in review.rating" v-bind:key="n">
+      </div>
+      <h3>{{ review.title }}</h3>
+      <p>{{ review.review }}</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+//  this is my model for the view
+//  name is required - used for the HTML tag
+  name: 'product-reviews',
+  data() {
+    return {
+      title: 'Cigar Parties for Dummies.',
+      description: 'Host and plan the perfect party',
+      reviews: [
+        {
+          reviewer: "Malcolm Gladwell",
+          title: "What a book!",
+          review:
+              "It certainly is a book. I mean, I can see that. Pages kept together with glue and there's writing on it, in some language.",
+          rating: 3,
+          isFavorite: false
+        },
+        {
+          reviewer: "Tim Ferriss",
+          title: "Had a cigar party started in less than 4 hours.",
+          review:
+              "It should have been called the four hour cigar party. That's amazing. I have a new idea for muse because of this.",
+          rating: 4,
+          isFavorite: false
+        },
+        {
+          reviewer: "Ramit Sethi",
+          title: "What every new entrepreneurs needs. A door stop.",
+          review:
+              "When I sell my courses, I'm always telling people that if a book costs less than $20, they should just buy it. If they only learn one thing from it, it was worth it. Wish I learned something from this book.",
+          rating: 1,
+          isFavorite: false
+        },
+        {
+          reviewer: "Gary Vaynerchuk",
+          title: "And I thought I could write",
+          review:
+              "There are a lot of good, solid tips in this book. I don't want to ruin it, but prelighting all the cigars is worth the price of admission alone.",
+          rating: 3,
+          isFavorite: false
+        }
+      ]
+    };
+  },
+  computed: {
+    averageRating() {
+      const sum = this.reviews.reduce((currentSum, review) => {
+        return currentSum + review.rating;
+      }, 0);
+
+      return sum / this.reviews.length;
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
